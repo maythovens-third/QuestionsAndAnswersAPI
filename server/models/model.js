bdf76a2d72f0ed5ff87c415f7151fab272b5c804
Richard Yu <richardyu123@gmail.com>
2021-05-13 16:51:15 -0700

    Clean up code, implement both get requests and the post request for adding question

--- server/models/model.js
+++ server/models/model.js
@@ -8,10 +8,8 @@ module.exports = {
     // if product_id parameter is specified
       if (product_id) {
         queryToUse = `SELECT * FROM questions WHERE product_id = ${product_id} LIMIT ${(page || 1) * (count || 5) - (count || 5)}, ${(count || 5)}`;
-        // INNER JOIN answers ON questions.id = answers.question_id
       } else {
         queryToUse = `SELECT * FROM questions LIMIT ${(page || 1) * (count || 5) - (count || 5)}, ${(count || 5)}`;
-        // INNER JOIN answers ON questions.id = answers.question_id
       }
 
       db.query(queryToUse, callback);
@@ -28,9 +26,5 @@ module.exports = {
     let queryToUse = `INSERT INTO questions (body, asker_name, asker_email, product_id)
                       VALUES (${body}, ${name}, ${email}, ${product_id})`;
     db.query(queryToUse, callback);
-  },
-  // addAnswer: (answer, question_id, callback) => {
-  //   const {body, name, email, photos} = answer;
-  //   let queryToUse = `INSERT INTO answers {body, answerer_name, anwerer_email, }`
-  // }
+  }
 };
